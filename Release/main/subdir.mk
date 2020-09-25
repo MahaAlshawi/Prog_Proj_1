b77564c806c2f8b11271d05edbe64cbe78a6c45b
Maha Alshawi <maha.alshawi.gr@dartmouth.edu>
2020-09-25 12:50:29 -0400

    First Commit with initial files setup

---
+++ Release/main/subdir.mk
@@ -0,0 +1,24 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../main/Prog_Proj_1.c 
+
+OBJS += \
+./main/Prog_Proj_1.o 
+
+C_DEPS += \
+./main/Prog_Proj_1.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+main/%.o: ../main/%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cygwin C Compiler'
+	gcc -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ main/Prog_Proj_1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Prog_Proj_1.c
+ Author      : Maha Alshawi
+ Version     :
+ Copyright   : @Dartmouth College
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
